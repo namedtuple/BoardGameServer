e91ec1d170329edb9cab9426f2614e428234d399
Brian Chipman <namedtuple@gmail.com>
2016-03-03 12:44:05 -0800

    Organization of GUI code - created update method, renamed variables, etc.

--- src/GUI.java
+++ src/GUI.java
@@ -7,21 +7,33 @@ import java.util.HashMap;
 public class GUI {
 
     // Fields
-    private JFrame jFrame;
+    private JFrame frame;
+    private JPanel boardPanel;
     private Board board;
     private HashMap<Pair<Integer, Integer>, GUIBoardCell> guiBoardMap;
+    private HashMap<Pair<Integer, Integer>, Character> boardMap;
 
     // Methods
     public GUI(Board board) {
         this.board = board;
-        HashMap<Pair<Integer, Integer>, Character> boardMap = board.getBoardMap();
-        this.jFrame = new JFrame("GUI");
+        boardMap = board.getBoardMap();
+        frame = new JFrame("GUI");
+        boardPanel = new JPanel();
+        guiBoardMap = new HashMap<>();
 
-        JPanel boardPanel = new JPanel();
         boardPanel.setBackground(Color.yellow);
         boardPanel.setLayout(new GridLayout(3, 3, 2, 2));
 
-        guiBoardMap = new HashMap<>();
+        frame.add(boardPanel, "Center");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(450, 300);
+        frame.setVisible(true);
+        frame.setResizable(false);
+
+        update();
+    }
+
+    public void update() {
         for (int xCol=1; xCol<=board.getSize(); ++xCol) {
             for (int yRow=1; yRow<=board.getSize(); ++yRow) {
                 Pair<Integer, Integer> pair = Pair.with(xCol, yRow);
@@ -37,13 +49,5 @@ public class GUI {
                 cell.add(label, gbc);
             }
         }
-
-        jFrame.add(boardPanel, "Center");
-
-        jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        jFrame.setSize(450, 300);
-        jFrame.setVisible(true);
-        jFrame.setResizable(false);
-
     }
 }
