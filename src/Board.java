00f4619776b741af19fdaa9fc899ec8a4bdb2379
Brian Chipman <namedtuple@gmail.com>
2016-03-02 20:06:07 -0800

    Added two 'makeMove' methods for Board class.  Added some demo code of using these methods in Main.main().

--- src/Board.java
+++ src/Board.java
@@ -12,17 +12,27 @@ public class Board {
     // Methods
     public Board(int size) {
         boardMap = new HashMap<>();
-        for (int row=1; row<=size; ++row) {
-            for (int col=1; col<=size; ++col) {
-                Pair<Integer, Integer> pair = Pair.with(row,col);
+        for (int xCol=1; xCol<=size; ++xCol) {
+            for (int yRow=1; yRow<=size; ++yRow) {
+                Pair<Integer, Integer> pair = Pair.with(xCol,yRow);
                 boardMap.put(pair, "_");
             }
         }
+    }
+
+    public void makeMove(Pair<Integer, Integer> location, String piece) {
+        boardMap.put(location, piece);
+    }
+
+    public void makeMove(int xCol, int yRow, String piece) {
+        boardMap.put(Pair.with(xCol, yRow), piece);
     }
 
     public void debugPrintBoardContents() {
         for (Map.Entry<Pair, String> entry : boardMap.entrySet()) {
             System.out.println(entry.getKey() + " -> " + entry.getValue());
         }
+        System.out.println();
     }
+
 }
--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,16 @@
+import org.javatuples.Pair;
+
 public class Main {
 
     public static void main(String[] args) {
         Board ticTacToeBoard = new Board(3);
+        ticTacToeBoard.debugPrintBoardContents();
+
+        ticTacToeBoard.makeMove(2,2, "X");
+        ticTacToeBoard.debugPrintBoardContents();
+
+        Pair<Integer, Integer> loc = Pair.with(1,1);
+        ticTacToeBoard.makeMove(loc, "O");
         ticTacToeBoard.debugPrintBoardContents();
     }
 }
