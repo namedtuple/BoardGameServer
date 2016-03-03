367356e3fcf7584e0e961b2e7b6b9180cec7e0d9
Brian Chipman <namedtuple@gmail.com>
2016-03-02 19:55:31 -0800

    Added javatuples library, created Board constructor and 'debugPrintBoardContents' method to output the empty board to the console, added 'Main' class with 'main' method.

--- src/Board.java
+++ src/Board.java
@@ -1,2 +1,28 @@
+import org.javatuples.Pair;
+
+import java.util.HashMap;
+import java.util.Map;
+
+
 public class Board {
+
+    // Fields
+    private HashMap<Pair, String> boardMap;
+
+    // Methods
+    public Board(int size) {
+        boardMap = new HashMap<>();
+        for (int row=1; row<=size; ++row) {
+            for (int col=1; col<=size; ++col) {
+                Pair<Integer, Integer> pair = Pair.with(row,col);
+                boardMap.put(pair, "_");
+            }
+        }
+    }
+
+    public void debugPrintBoardContents() {
+        for (Map.Entry<Pair, String> entry : boardMap.entrySet()) {
+            System.out.println(entry.getKey() + " -> " + entry.getValue());
+        }
+    }
 }
---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+
+    public static void main(String[] args) {
+        Board ticTacToeBoard = new Board(3);
+        ticTacToeBoard.debugPrintBoardContents();
+    }
+}
