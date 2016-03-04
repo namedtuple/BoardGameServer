b344f57381497e641053e4c7ecb3a9bf98110fcc
Brian Chipman <namedtuple@gmail.com>
2016-03-04 14:03:46 -0800

    Simple code formatting.

--- src/BoardGameServer.java
+++ src/BoardGameServer.java
@@ -5,45 +5,44 @@ import java.net.Socket;
 
 public class BoardGameServer {
 
-	public static final short PORT_NUM = 6666;
-	
-	private ServerSocket serverSocket; 
-	
-	public BoardGameServer() throws IOException{
-		serverSocket = new ServerSocket();
-		serverSocket.setReuseAddress(true);
-		serverSocket.bind(new InetSocketAddress(PORT_NUM));
-	}
+    public static final short PORT_NUM = 6666;
 
-	public void run() throws IOException {
-		while(true){
-			Socket newConnectionSocket = serverSocket.accept();
-			System.out.println("New connection from: " + newConnectionSocket.getRemoteSocketAddress());
-			
-			new BoardGameServerThread(newConnectionSocket).start();
-		}
-	}
-	
-	public void close(){
-		try {
-			serverSocket.close();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
-	
-	
-	public static void main(String[] args) {
-		BoardGameServer server = null;
-		try {
-			server = new BoardGameServer();
-			server.run();
-		} catch (IOException e) {
-			e.printStackTrace();
-		} finally {
-			if (server != null){
-				server.close();
-			}
-		}
-	}
+    private ServerSocket serverSocket;
+
+    public BoardGameServer() throws IOException {
+        serverSocket = new ServerSocket();
+        serverSocket.setReuseAddress(true);
+        serverSocket.bind(new InetSocketAddress(PORT_NUM));
+    }
+
+    public void run() throws IOException {
+        while (true) {
+            Socket newConnectionSocket = serverSocket.accept();
+            System.out.println("New connection from: " + newConnectionSocket.getRemoteSocketAddress());
+
+            new BoardGameServerThread(newConnectionSocket).start();
+        }
+    }
+
+    public void close() {
+        try {
+            serverSocket.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        BoardGameServer server = null;
+        try {
+            server = new BoardGameServer();
+            server.run();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (server != null) {
+                server.close();
+            }
+        }
+    }
 }
--- src/BoardGameServerThread.java
+++ src/BoardGameServerThread.java
@@ -6,35 +6,35 @@ import java.net.Socket;
 
 public class BoardGameServerThread extends Thread {
 
-	private Socket socket;
-	
-	private BufferedReader in;
-	private PrintWriter out;
-	
-	public BoardGameServerThread(Socket socket) throws IOException{
-		this.socket = socket;
-		in = new BufferedReader(new InputStreamReader(this.socket.getInputStream()));
-		out = new PrintWriter(this.socket.getOutputStream(), true);
-	}
-	
+    private Socket socket;
+
+    private BufferedReader in;
+    private PrintWriter out;
+
+    public BoardGameServerThread(Socket socket) throws IOException {
+        this.socket = socket;
+        in = new BufferedReader(new InputStreamReader(this.socket.getInputStream()));
+        out = new PrintWriter(this.socket.getOutputStream(), true);
+    }
+
     public void run() {
         send("Hello new client!");
-        while(true){
-        	try {
-				send("You said: " + recv());
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
+        while (true) {
+            try {
+                send("You said: " + recv());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
     }
-    
+
     //sends message to client
-    private void send(String message){
-    	out.println(message);
+    private void send(String message) {
+        out.println(message);
     }
-    
+
     //returns the next line of stream
-    private String recv() throws IOException{
-    	return in.readLine();
+    private String recv() throws IOException {
+        return in.readLine();
     }
 }
