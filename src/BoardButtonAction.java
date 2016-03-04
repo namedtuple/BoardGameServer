95cb0c4b0c7fd87fa984cc320b0136a2d25eb0fa
Brian Chipman <namedtuple@gmail.com>
2016-03-04 13:48:45 -0800

    Simplified chooseIcon method of BoardButtonAction.

--- src/BoardButtonAction.java
+++ src/BoardButtonAction.java
@@ -2,23 +2,38 @@ import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.util.HashMap;
 
 public class BoardButtonAction implements ActionListener {
 
+    // Fields
     private Board board;
+    private HashMap<Character, ImageIcon> imageHashMap;
 
+    // Methods
     public BoardButtonAction(Board board) {
         this.board = board;
+        loadImages();
+    }
+
+    public void loadImages() {
+        imageHashMap = new HashMap<>();
+        try {
+            imageHashMap.put('X', new ImageIcon(ImageIO.read(new File("img/x.png"))));
+            imageHashMap.put('O', new ImageIcon(ImageIO.read(new File("img/o.png"))));
+            imageHashMap.put('_', new ImageIcon(ImageIO.read(new File("img/blank.png"))));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
     /*
-	 * This will execute when a button object is clicked on the game board.
-	 * Gameboard will be properly updated to what is clicked as well.
-	 */ public void actionPerformed(ActionEvent e) {
+     * This will execute when a button object is clicked on the game board.
+     * Gameboard will be properly updated to what is clicked as well.
+     */ public void actionPerformed(ActionEvent e) {
         BoardButton command = (BoardButton) e.getSource();
         char iconChar = board.getTurn() == 0 ? 'X' : 'O';
         command.setIcon(chooseIcon(iconChar));
@@ -30,24 +45,8 @@ public class BoardButtonAction implements ActionListener {
         board.switchTurn();
     }
 
-    //helper method that will return an X or O icon
+    // Helper method that will return an X or O icon
     public ImageIcon chooseIcon(char piece) {
-        ImageIcon icon = new ImageIcon(new BufferedImage(50, 50, BufferedImage.TYPE_INT_ARGB));
-        if (piece == 'X') {
-            //icon = new ImageIcon(getClass().getResource("/../img/X.png"));
-            try {
-                icon = new ImageIcon(ImageIO.read(new File("img/x.png")));
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        } else if (piece == 'O') {
-            //icon = new ImageIcon(getClass().getResource("/../img/O.png"));
-            try {
-                icon = new ImageIcon(ImageIO.read(new File("img/o.png")));
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-        return icon;
+        return imageHashMap.get(piece);
     }
 }
