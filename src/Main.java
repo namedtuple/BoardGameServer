37c8e1d6a013bae0db25c946a36ee859059b7ebb
Brian Chipman <namedtuple@gmail.com>
2016-03-04 13:51:37 -0800

    Removed old debug code from Main.main.

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,8 @@
-import org.javatuples.Pair;
 
 public class Main {
 
     public static void main(String[] args) {
         Board ticTacToeBoard = new Board(3);
         GUI gui = new GUI(ticTacToeBoard);
-        //ticTacToeBoard.debugPrintBoardContentsBetter();
-        //
-        //ticTacToeBoard.makeMove(2,2, 'X');
-        //ticTacToeBoard.debugPrintBoardContentsBetter();
-        //gui.update();
-        //
-        //Pair<Integer, Integer> loc = Pair.with(1,1);
-        //ticTacToeBoard.makeMove(loc, 'O');
-        //ticTacToeBoard.debugPrintBoardContentsBetter();
-        //gui.update();
-
-
     }
 }
