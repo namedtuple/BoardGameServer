116b8a25cf009f7fc365145dd8dfe68e162101cd
Jonathan Yuen <pikahatonjon@gmail.com>
2016-03-09 14:58:18 -0800

    abstract class for game logic

---
+++ src/server/AbstractGameLogic.java
@@ -0,0 +1,9 @@
+
+public abstract class AbstractGameLogic {
+	public ServerThread currentPlayer;
+	public ServerThread opponent;
+	public Board board;
+	public abstract boolean makeMove();
+	public abstract boolean hasWinner();
+	public abstract boolean legalMove();
+}
