4ca2ad67dac7cbab31e874ce87d1c809ff44e730
Jonathan Yuen <pikahatonjon@gmail.com>
2016-03-09 14:08:43 -0800

    Revert "Merge remote-tracking branch 'jamitch1/master'"
    This reverts commit be7ccfda7726ee6e0a0d58870fdf7fd2558edd8f, reversing
    changes made to d182fe47efc82b8893eab1c06ab9f532de598bf4.

--- src/Main.java
+++
@@ -1,9 +0,0 @@
-
-public class Main {
-
-    public static void main(String[] args) {
-        Board ticTacToeBoard = new Board(3);
-        LoginScreen loginScreen = new LoginScreen("Login Screen");
-        GUI gui = new GUI(ticTacToeBoard);
-    }
-}
