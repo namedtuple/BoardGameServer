ebce9dd9f8f8aaad4f8997f7ac5ff56c9a48de33
Jonathan Yuen <pikahatonjon@gmail.com>
2016-03-09 15:22:34 -0800

    Blank CheckersLogic

---
+++ src/server/CheckersLogic.java
@@ -0,0 +1,22 @@
+
+public class CheckersLogic extends AbstractGameLogic{
+
+	@Override
+	public boolean makeMove() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean hasWinner() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean legalMove() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
