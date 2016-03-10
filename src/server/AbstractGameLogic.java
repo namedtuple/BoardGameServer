17d65744e7a981ccf955c76f492db8350a5370d5
Brian Chipman <namedtuple@gmail.com>
2016-03-09 17:41:43 -0800

    Changes to AbstractGameLogic and TicTacToeLogic to isolate game logic out from Server and ServerThread.

--- src/server/AbstractGameLogic.java
+++ src/server/AbstractGameLogic.java
@@ -9,15 +9,16 @@ public abstract class AbstractGameLogic implements GameLogicInterface {
 
     // Fields
     protected HashMap<Pair<Integer, Integer>, AbstractBoardTile> boardMap;  // change to new 'Board' class later
-    private ServerThread currentPlayer;
-    private ServerThread otherPlayer;
-    private final int LENGTH;
+    protected ServerThread currentPlayer;
+    protected ServerThread otherPlayer;
+    protected final int LENGTH;
 
     // Constructor
     public AbstractGameLogic(ServerThread currentPlayer, ServerThread otherPlayer, int length) {
         this.currentPlayer = currentPlayer;
         this.otherPlayer = otherPlayer;
         this.LENGTH = length;
+        setupBoardMap();
     }
 
     private void setupBoardMap() {
@@ -25,9 +26,21 @@ public abstract class AbstractGameLogic implements GameLogicInterface {
         for (int xCol = 1; xCol <= LENGTH; ++xCol) {
             for (int yRow = 1; yRow <= LENGTH; ++yRow) {
                 Pair<Integer, Integer> pair = Pair.with(xCol, yRow);
-                boardMap.put(pair, new TicTacToeBoardTile());
+                boardMap.put(pair, new TicTacToeBoardTile(pair));
             }
         }
+    }
+
+    public void debugPrintBoard() {
+        System.out.println("debugPrintBoard");
+        //for (int yRow = 1; yRow <= LENGTH; ++yRow) {
+        //    System.out.print("  ");
+        //    for (int xCol = 1; xCol <= LENGTH; ++xCol) {
+        //        char val = boardMap.get(Pair.with(xCol, yRow));
+        //        System.out.print(val + " ");
+        //    }
+        //    System.out.println();
+        //}
     }
 
 }
