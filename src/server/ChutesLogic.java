cfa9e6bc8aacb57b2d934b0e8a3d982ed3245980
Jonathan Yuen <pikahatonjon@gmail.com>
2016-03-09 16:45:24 -0800

    initial chuteslogic file, added win condition

---
+++ src/server/ChutesLogic.java
@@ -0,0 +1,31 @@
+package server;
+
+import org.javatuples.Pair;
+
+/**
+ * Created by Jonathan on 3/9/16.
+ */
+public class ChutesLogic extends AbstractGameLogic {
+    public ChutesLogic(ServerThread currentPlayer, ServerThread otherPlayer, int length) {
+        super(currentPlayer, otherPlayer, length);
+    }
+
+    public boolean legalMove(Pair<Integer, Integer> location, int roll) {
+        if(location.getValue1()!=1){
+            if(location.getValue0()-roll<1){
+                return false;
+          }
+        }
+        return true;
+    }
+
+    @Override
+    public boolean legalMove(Pair<Integer, Integer> location, ServerThread player) {
+        return false;
+    }
+
+    public boolean hasWinner() {
+        char hundred = boardMap.get(Pair.with(1,1));
+        return (hundred!='_');
+    }
+}
