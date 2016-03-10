5fe31025f5348a46d5e4e37102a766cd2b02386d
Jonathan Yuen <pikahatonjon@gmail.com>
2016-03-10 12:04:13 -0800

    does nothing

--- src/server/ChutesPlayer.java
+++ src/server/ChutesPlayer.java
@@ -1,7 +1,17 @@
 package server;
 
+import com.sun.corba.se.spi.oa.NullServant;
+
 /**
  * Created by Jonathan on 3/9/16.
  */
-public class ChutesPlayer {
+public class ChutesPlayer extends User {
+    public ChutesPlayer(String name) {
+        super(name);
+    }
+    private Pair<Integer,Integer> currentLocation = null;
+    public Pair<Integer,Integer> getlocation(){
+        return currentLocation;
+    }
+
 }
