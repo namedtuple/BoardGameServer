c069748f2771d9cf89490025b89b0d27635661b9
Brian Chipman <namedtuple@gmail.com>
2016-03-09 17:12:56 -0800

    Abstract tile, tictactoe tile.

--- src/server/AbstractBoardTile.java
+++ src/server/AbstractBoardTile.java
@@ -1,4 +1,29 @@
 package server;
 
+import org.javatuples.Pair;
+
 public abstract class AbstractBoardTile {
+
+    // Fields
+    private ServerThread occupant1;
+    private ServerThread occupant2;
+    private Pair<Integer, Integer> coordinates;
+
+    // Methods
+    public AbstractBoardTile(Pair<Integer, Integer> coordinates) {
+        this.coordinates = coordinates;
+    }
+
+    public void addOccupant(ServerThread serverThread) {
+        this.occupant1 = serverThread;
+    }
+
+    public ServerThread getOccupant() {
+        return occupant1;
+    }
+
+    public void setOccupant(ServerThread serverThread) {
+        occupant1 = serverThread;
+    }
+
 }
--- src/server/TicTacToeBoardTile.java
+++ src/server/TicTacToeBoardTile.java
@@ -1,4 +1,14 @@
 package server;
 
+import org.javatuples.Pair;
+
 public class TicTacToeBoardTile extends AbstractBoardTile {
+
+    // Fields
+
+    // Methods
+    public TicTacToeBoardTile(Pair<Integer, Integer> coordinates) {
+        super(coordinates);
+    }
+
 }
