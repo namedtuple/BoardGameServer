dc79508fd54675f6f8ac7d42affb61209faac6fc
Brian Chipman <namedtuple@gmail.com>
2016-03-09 17:16:49 -0800

    Fixed chutes logic.

--- src/server/ChutesLogic.java
+++ src/server/ChutesLogic.java
@@ -2,15 +2,11 @@ package server;
 
 import org.javatuples.Pair;
 
-/**
- * Created by Jonathan on 3/9/16.
- */
 public class ChutesLogic extends AbstractGameLogic {
     public ChutesLogic(ServerThread currentPlayer, ServerThread otherPlayer, int length) {
         super(currentPlayer, otherPlayer, length);
     }
 
-<<<<<<< HEAD
     public boolean legalMove(Pair<Integer, Integer> location, int roll) {
         if(location.getValue1()!=1){
             if(location.getValue0()-roll<1){
@@ -21,17 +17,12 @@ public class ChutesLogic extends AbstractGameLogic {
     }
 
     @Override
-=======
->>>>>>> origin/master
     public boolean legalMove(Pair<Integer, Integer> location, ServerThread player) {
         return false;
     }
 
     public boolean hasWinner() {
         char hundred = boardMap.get(Pair.with(1,1));
-<<<<<<< HEAD
         return (hundred!='_');
-=======
->>>>>>> origin/master
     }
 }
