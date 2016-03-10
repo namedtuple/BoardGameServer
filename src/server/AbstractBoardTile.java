547fe9ac9025ded3d7d888798ca95c7df2b7f682
Jonathan Yuen <pikahatonjon@gmail.com>
2016-03-10 13:34:10 -0800

    undo previous override

--- src/server/AbstractBoardTile.java
+++ src/server/AbstractBoardTile.java
@@ -1,6 +1,21 @@
 package server;
 
 public abstract class AbstractBoardTile {
-    
+    private ServerThread occupant1;
+    private ServerThread occupant2;
+    private Pair<Integer, Integer> coordinates;
+    // Methods
+    public AbstractBoardTile(Pair<Integer, Integer> coordinates) {
+        this.coordinates = coordinates;
+    }
+    public void addOccupant(ServerThread serverThread) {
+        this.occupant1 = serverThread;
+    }
+    public ServerThread getOccupant() {
+        return occupant1;
+    }
+   public void setOccupant(ServerThread serverThread) {
+        occupant1 = serverThread;
+   }
 
 }
