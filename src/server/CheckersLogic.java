0f482909a9ee25d1d59d68ebc0888a110d45005a
Brian Chipman <namedtuple@gmail.com>
2016-03-10 10:51:56 -0800

    Commented out CheckersLogic and ChutesLogic.

--- src/server/CheckersLogic.java
+++ src/server/CheckersLogic.java
@@ -1,23 +1,23 @@
-package server;
-
-public class CheckersLogic extends AbstractGameLogic{
-
-	@Override
-	public boolean makeMove() {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public boolean hasWinner() {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public boolean legalMove() {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-}
+//package server;
+//
+//public class CheckersLogic extends AbstractGameLogic{
+//
+//	@Override
+//	public boolean makeMove() {
+//		// TODO Auto-generated method stub
+//		return false;
+//	}
+//
+//	@Override
+//	public boolean hasWinner() {
+//		// TODO Auto-generated method stub
+//		return false;
+//	}
+//
+//	@Override
+//	public boolean legalMove() {
+//		// TODO Auto-generated method stub
+//		return false;
+//	}
+//
+//}
