b36bcd4379c5fe70178148fd5c7dad661e6394b9
Jonathan Yuen <pikahatonjon@gmail.com>
2016-03-10 12:03:31 -0800

    chutes board tile

--- src/server/ChutesBoardTile.java
+++ src/server/ChutesBoardTile.java
@@ -1,8 +1,21 @@
 package server;
 
+import org.javatuples.Pair;
+
 /**
  * Created by Jonathan on 3/9/16.
  */
 public class ChutesBoardTile extends AbstractBoardTile{
+    public ChutesBoardTile() {
+        //super(coordinates);
+    }
+    public ChutesBoardTile(Pair<Integer, Integer> coordinates) {
+        super(coordinates);
+    }
+    public void moveTo(Pair<Integer,Integer> coordinates){
+
+    }
+
+
 
 }
