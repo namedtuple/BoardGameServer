a769aa9738f3a7c9dfac008a55b1203ef212eb82
Jonathan Yuen <pikahatonjon@gmail.com>
2016-03-15 14:20:54 -0700

    removing for now
    removing for now

--- src/server/ChutesBoardTile.java
+++ src/server/ChutesBoardTile.java
@@ -1,22 +1,22 @@
+//package server;
+//
+//import org.javatuples.Pair;
+//
+///**
+// * Created by Jonathan on 3/9/16.
+// */
+//public class ChutesBoardTile extends AbstractBoardTile{
 //    public ChutesBoardTile() {
-package server;
-
-import org.javatuples.Pair;
-
-/**
- * Created by Jonathan on 3/9/16.
- */
-public class ChutesBoardTile extends AbstractBoardTile{
-    public ChutesBoardTile() {
-        //super(coordinates);
-    }
-    public ChutesBoardTile(Pair<Integer, Integer> coordinates) {
-        super(coordinates);
-    }
-    public void moveTo(Pair<Integer,Integer> coordinates){
-
-    }
-
-
-
-}
+//        super();
+//        //super(coordinates);
+//    }
+//    public ChutesBoardTile(Pair<Integer, Integer> coordinates) {
+//        super(coordinates);
+//    }
+//    public void moveTo(Pair<Integer,Integer> coordinates){
+//
+//    }
+//
+//
+//
+//}
