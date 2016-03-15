28d949f32292ecafbb3ca77ba9285d57bba09099
Jonathan Yuen <pikahatonjon@gmail.com>
2016-03-15 14:19:30 -0700

    organizing chutes to fit design
    making chutes respond to messages. putting in things needed at the
    start of the game

--- src/server/ChutesPlayer.java
+++ src/server/ChutesPlayer.java
@@ -1,17 +1,27 @@
-//package server;
-//
-//import com.sun.corba.se.spi.oa.NullServant;
-//
-///**
-// * Created by Jonathan on 3/9/16.
-// */
-//public class ChutesPlayer extends User {
-//    public ChutesPlayer(String name) {
-//        super(name);
-//    }
-//    private Pair<Integer,Integer> currentLocation = null;
-//    public Pair<Integer,Integer> getlocation(){
-//        return currentLocation;
-//    }
-//
-//}
+package server;
+
+import com.sun.corba.se.spi.oa.NullServant;
+import com.sun.tools.javac.util.Pair;
+
+/**
+ * Created by Jonathan on 3/9/16.
+ */
+public class ChutesPlayer extends User {
+    private int x;
+    private int y;
+    public ChutesPlayer(String name) {
+        super(name);
+        //x=0;
+        //y=10;
+    }
+    private Pair<Integer,Integer> currentLocation = null;
+
+    public ChutesPlayer() {
+        super();
+    }
+
+    public Pair<Integer,Integer> getLocation(){
+        return currentLocation;
+    }
+    public void setLocation(Pair<Integer, Integer> location){ currentLocation = location;}
+}
--- src/server/User.java
+++ src/server/User.java
@@ -9,6 +9,10 @@ public class User {
 		this.name = name;
 	}
 
+	public User() {
+
+	}
+
 	public String getName() {
 		return name;
 	}
