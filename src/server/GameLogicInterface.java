1a2d53fc31cae8ca601f841ba97febba2f335296
Jonathan Yuen <pikahatonjon@gmail.com>
2016-03-15 20:16:33 -0700

    overloaded legalMove function

--- src/server/GameLogicInterface.java
+++ src/server/GameLogicInterface.java
@@ -4,6 +4,7 @@ import org.javatuples.Pair;
 
 public interface GameLogicInterface {
     boolean legalMove(Pair<Integer, Integer> location, ServerThread player);
+    boolean legalMove(ServerThread player);
     boolean hasWinner();
     boolean tied();
 }
