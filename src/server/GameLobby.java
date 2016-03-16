bd964097f3f9a818b3bac96fa520c46deb5258e5
Jonathan Yuen <pikahatonjon@gmail.com>
2016-03-15 21:43:10 -0700

    .

--- src/server/GameLobby.java
+++ src/server/GameLobby.java
@@ -8,87 +8,88 @@ import java.util.List;
 
 public class GameLobby {
 
-    // Fields
+	// Fields
 	private String lobbyName;
-    private List<String> list;
+	private List<String> list;
 
-    // Methods
+	// Methods
 	public GameLobby(String lobbyName) {
-        this.lobbyName = lobbyName;
-        list = new ArrayList<>();
+		this.lobbyName = lobbyName;
+		list = new ArrayList<>();
 	}
 
-	public void addUser(String userName){
+	public void addUser(String userName) {
 		list.add(userName);
-        debugPrintLobbyContents();
+		debugPrintLobbyContents();
 	}
 
-	public void removeUser(String userName){
+	public void removeUser(String userName) {
 		list.remove(userName);
-        debugPrintLobbyContents();
+		debugPrintLobbyContents();
 	}
-	public String getLobbyName(){
+
+	public String getLobbyName() {
 		return lobbyName;
 	}
 
 
 	//Can be used to send contents of waiting list over socket
-    @Override
+	@Override
 	public String toString() {
 		StringBuilder str = new StringBuilder();
-        str.append(lobbyName).append(" ");
-		for(String userName : list){
+		str.append(lobbyName).append(" ");
+		for (String userName : list) {
 			str.append(userName);
 			str.append(" ");
 		}
 		return str.toString();
 	}
 
-	public void startGame(ServerThread player1, ServerThread player2){
-		System.out.println("CURRENT GAME IS"+ lobbyName);
+	public void startGame(ServerThread player1, ServerThread player2) {
+		System.out.println("CURRENT GAME IS" + lobbyName);
 		String p1 = player1.getUserName();
 		String p2 = player2.getUserName();
-		if (lobbyName.equalsIgnoreCase("Chutes-N-Ladders")){
+		if (lobbyName.equalsIgnoreCase("Chutes-N-Ladders")) {
 			player1.send("CHUTE");
 
-		}
-		else if (lobbyName.equalsIgnoreCase("Tic-Tac-Toe")){
+		} else if (lobbyName.equalsIgnoreCase("Tic-Tac-Toe")) {
 			TicTacToeLogic newGame = new TicTacToeLogic(player1, player2, 3);
-
 
 
 			player1.send("WELCOME " + p1 + " X " + p2 + " O "); // 'WELCOME username1 X username2 O '
 			player1.setOpponent(player2);
 			player1.setGame(newGame);
 
-        player1.send(new Request(Command.WELCOME, p1 + " X " + p2 + " O ")); // 'WELCOME username1 X username2 O '
-		player1.setOpponent(player2);
-		player1.setGame(newGame);
+			player1.send(new Request(Command.WELCOME, p1 + " X " + p2 + " O ")); // 'WELCOME username1 X username2 O '
+			player1.setOpponent(player2);
+			player1.setGame(newGame);
 
-        player2.send(new Request(Command.WELCOME, p2 + " O " + p1 + " X ")); // 'WELCOME username2 O username1 X '
-		player2.setOpponent(player1);
-		player2.setGame(newGame);
+			player2.send(new Request(Command.WELCOME, p2 + " O " + p1 + " X ")); // 'WELCOME username2 O username1 X '
+			player2.setOpponent(player1);
+			player2.setGame(newGame);
 
+		}
 	}
 
-    private void debugPrintLobbyContents() {
-        System.out.println("-----------------------------------------------------------------------------");
-        String lobbyContentsMessage = lobbyName + " lobby contents updated: ";
-        if (list.size() == 0) {
-            lobbyContentsMessage += "<empty>";
-        } else {
-            for (String user : list) {
-                lobbyContentsMessage += user + " ";
-            }
-        }
-        System.out.println(lobbyContentsMessage);
-    }
+	private void debugPrintLobbyContents() {
+		System.out.println("-----------------------------------------------------------------------------");
+		String lobbyContentsMessage = lobbyName + " lobby contents updated: ";
+		if (list.size() == 0) {
+			lobbyContentsMessage += "<empty>";
+		} else {
+			for (String user : list) {
+				lobbyContentsMessage += user + " ";
+			}
+		}
+		System.out.println(lobbyContentsMessage);
+	}
 
-    //public void handleRequest(Request request) { // TODO - Make a copy of Request and Command classes and put in server package.
-    //    String[] tokens = request.getTokens();
-    //    Command command = request.getCommand();
-    //    switch(command) {
-    //        case JOIN:
-    //    }
-    //}
+	//public void handleRequest(Request request) { // TODO - Make a copy of Request and Command classes and put in server package.
+	//    String[] tokens = request.getTokens();
+	//    Command command = request.getCommand();
+	//    switch(command) {
+	//        case JOIN:
+	//    }
+	//}
 }
+
