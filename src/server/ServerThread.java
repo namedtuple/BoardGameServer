b66786f80f62dc81f023c6120249b849f8a1aa3c
Jonathan Yuen <pikahatonjon@gmail.com>
2016-03-15 20:16:55 -0700

    intercept server message to handle different message for different games

--- src/server/ServerThread.java
+++ src/server/ServerThread.java
@@ -68,10 +68,27 @@ public class ServerThread extends Thread {
                 }
                 //handling moving for game
                 else if (firstToken.equals("MOVE")) {
-                    if (game.legalMove(extractPosition(msg), this)) {
-                        send("VALID_MOVE");
-                        send(game.hasWinner() ? "VICTORY" : game.tied() ? "TIE" : "");
+                    if (lobby.getLobbyName().equalsIgnoreCase("Chutes-N-Ladders")){
+                        if (game.legalMove(this)){
+                            send("VALID_MOVE");
+                            send(game.hasWinner() ? "VICTORY" : game.tied() ? "TIE" : "");
+                        }
                     }
+                    else if (lobby.getLobbyName().equalsIgnoreCase("Tic-Tac-Toe")){
+                        if (game.legalMove(extractPosition(msg), this)) {
+                            send("VALID_MOVE");
+                            send(game.hasWinner() ? "VICTORY" : game.tied() ? "TIE" : "");
+                        }
+                    }
+                    else if (lobby.getLobbyName().equalsIgnoreCase("Checkers")){
+                        if (game.legalMove(extractPosition(msg), this)) {
+                            send("VALID_MOVE");
+                            send(game.hasWinner() ? "VICTORY" : game.tied() ? "TIE" : "");
+                        }
+                    }
+
+
+
                 }
 
                 else if (firstToken.equals("LOGOUT")) {
