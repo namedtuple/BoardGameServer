3367705765634d945ec02b0646a54f4c35020bb1
NicodySazawa <cbsauer@uci.edu>
2016-03-16 17:15:20 -0700

    slight modification since profile won't need user password

--- src/server/AccountAuthenticator.java
+++ src/server/AccountAuthenticator.java
@@ -65,7 +65,6 @@ public class AccountAuthenticator {
 			if(rs.next())
 			{	
 				String profileInfo = rs.getString(1) + ", " 
-					 + rs.getString(2) + ", "
 					 + rs.getString(3) + ", "
 					 + rs.getString(4) + ", " 
 					 + rs.getInt(5) + ", "
